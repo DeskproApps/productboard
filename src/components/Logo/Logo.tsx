de9aee1a2ed92362289b9eba51792dd7b6baccbe
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-02-07 11:27:48 +0000

    update Logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,13 @@
 function Logo() {
-    return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 73.323 64" />
+    return (
+        <svg width='800px' height='800px' viewBox='0 -44 256 256' version='1.1' xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMidYMid'>
+            <g>
+                <polygon fill='#FF2638' points='85.326959 83.9967251 170.653918 167.99345 0 167.99345' />
+                <polygon fill='#FFC600' points='0 0 85.326959 83.9967251 170.653918 0' />
+                <polygon fill='#0079F2' points='85.3406727 83.9967251 170.667632 167.99345 255.994591 83.9967251 170.667632 0' />
+            </g>
+        </svg>
+    );
 };
 
 export default Logo;
