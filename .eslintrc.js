b3279abeb4f193e400b664b826ccdd6b5895aff2
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-03-05 15:12:15 +0000

    fix indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
   root: true,
   ignorePatterns: [
-      "**/*.js",
-      ".dist/**/*",
-      "build/**/*",
-      "dist/**/*"
+    "**/*.js",
+    ".dist/**/*",
+    "build/**/*",
+    "dist/**/*"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-      project: "./tsconfig.json"
+    project: "./tsconfig.json"
   },
   plugins: [
-      "@typescript-eslint"
+    "@typescript-eslint"
   ],
   extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended-type-checked",
-      "plugin:react-hooks/recommended",
-      "prettier"
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "prettier"
   ],
   rules: {
-      "@typescript-eslint/no-empty-function": "off",
-      "@typescript-eslint/no-unused-vars": ["warn", {
-          ignoreRestSiblings: true,
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_"
-      }],
-      "no-console": "warn"
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", {
+      ignoreRestSiblings: true,
+      argsIgnorePattern: "^_",
+      varsIgnorePattern: "^_"
+    }],
+    "no-console": "warn"
   }
 };
