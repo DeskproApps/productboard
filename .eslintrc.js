51f90f0295a020a98c48b5065b628c8eaa675673
tidbit1 <194808961+tidbit1@users.noreply.github.com>
2025-03-05 15:11:25 +0000

    halve indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    root: true,
-    ignorePatterns: [
-        "**/*.js",
-        ".dist/**/*",
-        "build/**/*",
-        "dist/**/*"
-    ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        project: "./tsconfig.json"
-    },
-    plugins: [
-        "@typescript-eslint"
-    ],
-    extends: [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended-type-checked",
-        "plugin:react-hooks/recommended",
-        "prettier"
-    ],
-    rules: {
-        "@typescript-eslint/no-empty-function": "off",
-        "@typescript-eslint/no-unused-vars": ["warn", {
-            ignoreRestSiblings: true,
-            argsIgnorePattern: "^_",
-            varsIgnorePattern: "^_"
-        }],
-        "no-console": "warn"
-    }
+  root: true,
+  ignorePatterns: [
+      "**/*.js",
+      ".dist/**/*",
+      "build/**/*",
+      "dist/**/*"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+      project: "./tsconfig.json"
+  },
+  plugins: [
+      "@typescript-eslint"
+  ],
+  extends: [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended-type-checked",
+      "plugin:react-hooks/recommended",
+      "prettier"
+  ],
+  rules: {
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", {
+          ignoreRestSiblings: true,
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_"
+      }],
+      "no-console": "warn"
+  }
 };
