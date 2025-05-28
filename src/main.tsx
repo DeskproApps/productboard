f0b9c8d10697e7d857eff069039571ce78235aec
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 15:16:17 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
-import * as Sentry from '@sentry/react';
 import './instrument';
 import { StrictMode, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
-import { ErrorBoundary } from 'react-error-boundary';
 import { DeskproAppProvider, LoadingSpinner } from '@deskpro/app-sdk';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '@/query';
@@ -13,25 +11,26 @@ import App from './App';
 import '@deskpro/deskpro-ui/dist/deskpro-ui.css';
 import '@deskpro/deskpro-ui/dist/deskpro-custom-icons.css';
 import './main.css';
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
-  onRecoverableError: Sentry.reactErrorHandler(),
+  onRecoverableError: reactErrorHandler(),
 });
 
 root.render((
-    <StrictMode>
-        <DeskproAppProvider>
-            <HashRouter>
-                <QueryClientProvider client={queryClient}>
-                    <Suspense fallback={<LoadingSpinner />}>
-                        <Sentry.ErrorBoundary fallback={<ErrorPage />}>
-                            <StoreProvider>
-                                <App />
-                            </StoreProvider>
-                        </Sentry.ErrorBoundary>
-                    </Suspense>
-                </QueryClientProvider>
-            </HashRouter>
-        </DeskproAppProvider>
-    </StrictMode>
+  <StrictMode>
+    <DeskproAppProvider>
+      <HashRouter>
+        <QueryClientProvider client={queryClient}>
+          <Suspense fallback={<LoadingSpinner />}>
+            <ErrorBoundary fallback={<ErrorPage />}>
+              <StoreProvider>
+                <App />
+              </StoreProvider>
+            </ErrorBoundary>
+          </Suspense>
+        </QueryClientProvider>
+      </HashRouter>
+    </DeskproAppProvider>
+  </StrictMode>
 ));
